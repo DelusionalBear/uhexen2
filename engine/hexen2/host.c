c15389eab8795f848437b75183da9920a1337440
dustin <samko.dustin@gmail.com>
2021-07-13 10:14:15 -0400

    host_maxfps fix

--- engine/hexen2/host.c
+++ engine/hexen2/host.c
@@ -79,7 +79,6 @@ cvar_t	samelevel = {"samelevel", "0", CVAR_NONE};
 cvar_t	noexit = {"noexit", "0", CVAR_NOTIFY|CVAR_SERVERINFO};
 
 cvar_t	developer = {"developer", "0", CVAR_ARCHIVE};
-cvar_t  host_maxfps = {"host_maxfps", "72", CVAR_ARCHIVE};
 cvar_t	skill = {"skill", "1", CVAR_NONE};		// 0 - 3
 cvar_t	coop = {"coop", "0", CVAR_NONE};		// 0 or 1
 cvar_t	deathmatch = {"deathmatch", "0", CVAR_NONE};	// 0, 1, or 2
